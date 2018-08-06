4f9ea6c542e1030583864ea7119755f5d9b197e5
mylinux <seksant>
2018-08-06 21:29:36 +0700

    add line to install nvm

--- base_software.sh
+++ base_software.sh
@@ -5,8 +5,15 @@ molly-guard \
 terminator \
 sshpass \
 screen \
-ntpdate
+ntpdate \
+curl \
+mysql-server
 
+#Sync time
 sudo ntpdate pool.ntp.org
+
+#Setup molly-guard
 sudo sed -i 's/#ALWAYS_QUERY_HOSTNAME=true/ALWAYS_QUERY_HOSTNAME=true/' /etc/molly-guard/rc
 
+#install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
